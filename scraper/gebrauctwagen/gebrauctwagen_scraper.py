b77d9c57ffc5ed8dec2475e223b49fe8c5372af7
seboo1995 <sebair_selmani@yahoo.com>
2023-07-22 20:42:40 +0200

    fixed gitignore

---
+++ scraper/gebrauctwagen/gebrauctwagen_scraper.py
@@ -0,0 +1,77 @@
+import configparser
+import requests
+import time
+from bs4 import BeautifulSoup
+import pandas as pd
+import os
+from datetime import datetime
+
+# prepare env
+
+
+
+
+SECTIONS = ['css_selectors','options']
+
+
+
+def load_config_params() -> dict:
+    params:dict = {}
+    config = configparser.ConfigParser()
+    config.read('scraper/gebrauctwagen/gebrauchtWagen_config.ini')
+    sections = config.sections()
+    for section in SECTIONS:
+        if section not in sections:
+            raise ValueError(f'Missing sections: {section}')
+    params['css_selectors'] = dict(config['css_selectors'])
+    params['options'] = dict(config['options'])
+    return params
+
+
+class GebrauchtWagenScraper:
+    def __init__(self):
+        self.params = load_config_params()
+        self.res_list = []
+    
+    def _create_requests(self):
+        '''
+        This function will produce the responses
+        '''
+        max_pages = int(self.params['options']['max_pages'])
+        timeout = int(self.params['options']['timeout_in_seconds'])
+        base_url = self.params['options']['base_url']
+        responses_list = []
+        for i in range(1,max_pages):
+            url = base_url+str(i)
+            response = requests.get(url)
+            if response.status_code != 200:
+                raise ValueError(f'Status code for page{str(i)} was {response.status_code}')
+                continue
+            soup = BeautifulSoup(response.text)
+            responses_list.append(soup)
+            time.sleep(timeout)
+        return responses_list
+    
+    def get_data_from_responeses(self):
+        responses_soups = self._create_requests()
+        css_selectors = self.params['css_selectors']
+        temp_path = self.params['options']['temp_path']
+        # first get indiividual ads
+        for page in responses_soups:
+            print('Page: '+ str(responses_soups.index(page)))
+            all_ads_in_page = page.select(css_selectors['ad_selector'])
+            for individual_ad in all_ads_in_page:
+                temp_dict = {}
+                for col_name,css_selector in css_selectors.items():
+                    if col_name == 'ad_selector':
+                        continue
+                    temp_dict[col_name] = individual_ad.select(css_selector)[0].text
+                self.res_list.append(temp_dict)
+        df = pd.DataFrame(self.res_list)
+        if  not os.path.exists(temp_path):
+            os.mkdir(temp_path)
+
+        df.to_csv(f'{os.path.join(temp_path,self.make_fileName())}')
+        return f'{os.path.join(temp_path,self.make_fileName())}'
+    def make_fileName(self):
+        return datetime.now().strftime('%d-%b-%y') +'.csv'
